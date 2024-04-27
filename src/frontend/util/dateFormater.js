fc7e43383b4ea54a9fe5ae3b85b9fec624211881
Eduardo Souza <eduard.sg@icloud.com>
2024-04-27 00:54:25 -0300

    View user reserves + fix setTimeout + Add time + New style

--- src/frontend/util/dateFormater.js
+++ src/frontend/util/dateFormater.js
@@ -3,11 +3,15 @@ export function brazilianDate(date) {
 
     const addZeroLeft = value => value < 10 ? `0${value}` : value;
 
-    const day = addZeroLeft(data.getDate() + 1);
+    const day = addZeroLeft(data.getDate());
     const month = addZeroLeft(data.getMonth() + 1);
     const year = data.getFullYear();
+    const hours = addZeroLeft(data.getHours());
+    const minutes = addZeroLeft(data.getMinutes());
+    const seconds = addZeroLeft(data.getSeconds());
+
+    const dateTimeFormater = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+    return dateTimeFormater;
     
-    const dateFormater = `${day}/${month}/${year}`;
-    
-    return dateFormater;
 }
